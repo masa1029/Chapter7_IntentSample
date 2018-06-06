a8eae00b9bd562f54dab0787d7e02f0100d28fc5
masa1029 <masaco1290@gmail.com>
2018-06-06 10:17:25 +0900

    Initial commit

---
+++ app/src/main/java/com/masaki/intentsample/MenuThanksActivity.java
@@ -0,0 +1,28 @@
+package com.masaki.intentsample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuThanksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_thanks);
+
+       Intent intent = getIntent();
+
+       String menuName = intent.getStringExtra("menuName");
+       String menuPrice= intent.getStringExtra("menuPrice");
+
+        TextView tvMenuName = findViewById(R.id.tvMenuName);
+        TextView tvMenuPrice = findViewById(R.id.tvMenuPrice);
+
+    }
+    public void onBackButtonClick(View view){
+        finish();
+    }
+}
